ac72103a9f50eeaebfd1084b4cc81641bede18c3
bashirafarhin <farhinbashira@gmail.com>
2025-08-08 12:06:35 +0530

    enable cookie to pass in production

--- server/src/utils/generateTokens.ts
+++ server/src/utils/generateTokens.ts
@@ -11,7 +11,7 @@ export const sendAccessToken = (res: Response, token: string) => {
   res.cookie("accessToken", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: false,
+    sameSite: "none",
     maxAge: 30 * 60 * 1000,
   });
 };
